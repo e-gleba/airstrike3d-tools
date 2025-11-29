fe45019c459777b3e0d3e637e3eb92b81277bedf
evgeniy.gleba <i@egleba.ru>
2025-11-29 15:49:38 +0300

    mdl -> obj

--- cmake/cpm/cpm_stb-config.cmake
+++ cmake/cpm/cpm_stb-config.cmake
@@ -3,11 +3,14 @@ cpmaddpackage(
     stb
     GITHUB_REPOSITORY
     nothings/stb
+    SYSTEM
+    ON
+    GIT_SHALLOW
+    ON
     GIT_TAG
     master
     DOWNLOAD_ONLY
-    TRUE
-)
+    TRUE)
 
 add_library(stb INTERFACE)
 target_include_directories(stb SYSTEM INTERFACE ${stb_SOURCE_DIR})
