4ddde005031359e22ce609952035ceaa69cf88bc
evgeniy.gleba <i@egleba.ru>
2025-12-06 21:10:28 +0300

    fix format

--- src/engine/private/main.cpp
+++ src/engine/private/main.cpp
@@ -12,13 +12,13 @@
 namespace
 {
 
-/// Build game library path for current platform
 [[nodiscard]] std::filesystem::path get_game_library_path()
 {
     const char* base_path = SDL_GetBasePath();
-    if (base_path == nullptr) {
+    if (base_path == nullptr)
+    {
         return {};
-}
+    }
 
     return std::filesystem::path(base_path) /
            euengine::platform::game_library_name();
@@ -30,8 +30,8 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 {
     using namespace euengine;
 
-    const window_settings window{
-        .title     = "airstrike3d",
+    constexpr window_settings window {
+        .title     = "euengine",
         .width     = 1280,
         .height    = 720,
         .mode      = window_mode::windowed,
@@ -40,12 +40,12 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
         .high_dpi  = true,
     };
 
-    const engine_config config{
+    const engine_config config {
         .window   = window,
         .game_lib = get_game_library_path(),
     };
 
-    engine eng;
+    engine eng {};
 
     if (!eng.init(config))
     {
@@ -54,7 +54,6 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
     }
 
     eng.run();
-    // shutdown() called automatically by destructor
 
     return EXIT_SUCCESS;
 }
